c9d72f644c83ce171b76c360a1c64daeaa16e086
Luke <pete9348@umn.edu>
2015-08-19 15:07:04 -0500

    Added icons, updated license, fixed popup bug, isolated db

--- food-trucks.js
+++ food-trucks.js
@@ -2,12 +2,18 @@ Trucks = new Mongo.Collection("trucks");
 
 if (Meteor.isClient) {
   // This code only runs on the client
+  var trucks = Trucks.find({}, {
+    limit: 3,
+    sort: {createdAt: -1}
+  });
+  //createdAt: new Date().toDateString()
 
   Template.map.rendered = function() {
     L.Icon.Default.imagePath = 'packages/bevanhunt_leaflet/images';
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
+
     } else {
       map = L.map('map', {doubleClickZoom: false}).setView([44.9778, -93.2650], 12);
       setMap();
@@ -15,7 +21,7 @@ if (Meteor.isClient) {
 
     function showPosition(position) {
       map = L.map('map', {doubleClickZoom: false}).setView([position.coords.latitude, position.coords.longitude], 16);
-      setMap()
+      setMap();
     }
 
     function setMap(){
@@ -26,21 +32,27 @@ if (Meteor.isClient) {
         document.getElementById("lat").value = event.latlng.lat;
         document.getElementById("long").value = event.latlng.lng;
       });
+
+      var truckIcon = L.icon({
+          iconUrl: '/marker-icon.png',
+          //shadowUrl: 'marker-icon.png',
+
+          iconSize:     [30, 43], // size of the icon
+          iconAnchor:   [15, 42], // point of the icon which will correspond to marker's location
+          popupAnchor:  [0, -20] // point from which the popup should open relative to the iconAnchor
+      });
+
+      trucks.forEach(function (truck) {
+        marker = new L.marker([truck.lat,truck.long], {icon:truckIcon})
+        .bindPopup("<b>"+truck.name+" - "+truck.type+"</b><br>"+truck.start.toString()+" - "+truck.end.toString())
+        .addTo(map);
+      });
     }
   }
 
   Template.body.helpers({
     trucks: function () {
-      return Trucks.find({createdAt: new Date().toDateString()}, {
-        limit: 2,
-        sort: {createdAt: -1}
-      });
-    },
-    markers: function () {
-      return Trucks.find({createdAt: new Date().toDateString()}, {
-        limit: 2,
-        sort: {createdAt: -1}
-      });
+      return trucks;
     }
   });
 
@@ -53,16 +65,7 @@ if (Meteor.isClient) {
       var lat = parseFloat(event.target.lat.value);
       var long = parseFloat(event.target.long.value);
 
-      Trucks.insert({
-        name: name,
-        type: type,
-        start: startTime,
-        end: endTime,
-        lat: lat,
-        long: long,
-        createdAt: new Date().toDateString() // current time
-
-      });
+      Meteor.call("addTruck", name, type, startTime, endTime, lat, long);
 
       // Clear form
       event.target.name.value = "";
@@ -75,3 +78,24 @@ if (Meteor.isClient) {
     }
   });
 }
+
+Meteor.methods({
+  addTruck: function (name, type, startTime, endTime, lat, long) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Trucks.insert({
+      name: name,
+      type: type,
+      start: startTime,
+      end: endTime,
+      lat: lat,
+      long: long,
+      createdAt: new Date(), // current time
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  }
+});
